54f7fc5d755ca90f60e9d3540e696d5e4f5ec55c
BlueInt32 <simon.budin@gmail.com>
2019-12-12 06:35:45 +0100

    try out sqlprovider stuff

--- back/Shelter.Api/Program.fs
+++ back/Shelter.Api/Program.fs
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
-
 open System
+open FSharp.Data.Sql
+
+[<Literal>]
+let ResolutionPath = __SOURCE_DIRECTORY__ + "/libraries/"
+
+[<Literal>] // Relative to resolutionPath:
+let ConnStr = @"Filename=" + __SOURCE_DIRECTORY__ + @"/../shelter.db"
+
+// In case of dependency error, note that SQLite dependencies are processor architecture dependant
+type HR = SqlDataProvider< Common.DatabaseProviderTypes.SQLITE, ConnStr, ResolutionPath = ResolutionPath, SQLiteLibrary=Common.SQLiteLibrary.MicrosoftDataSqlite>
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let ctx = HR.GetDataContext()
+    let employeesFirstName =
+        query {
+            for emp in ctx.Main.Gems do
+            select emp.Title
+        } |> Seq.head
+
+    printfn "Hello %s!" employeesFirstName
     0 // return an integer exit code
