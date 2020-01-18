755f251ee1d813eb8629e77c00961e5ed2624a53
BlueInt32 <simon.budin@gmail.com>
2020-01-18 12:37:30 +0100

    UI - Impl. flex style on gemsList

--- Shelter.front/src/views/components/GemsList.vue
+++ Shelter.front/src/views/components/GemsList.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="gemsList">
-    <p v-for="gem in gems" :key="gem.id">
-      {{ gem.title }} - {{ gem.tags.map(t => t.label).join() }}
-      <router-link :to="{ name: 'viewGem', params: { gemId: gem.id } }"
-        >view</router-link
-      >
-    </p>
+  <div class="gemsList__container">
+    <div class="gemsList__item" v-for="gem in gems" :key="gem.id">
+      <div class="gemsList__innerBox">
+        <h2>{{ gem.title }}</h2>
+        <p class="gemsList__tagsList">
+          {{ gem.tags.map(t => t.label).join(', ') }}
+        </p>
+        <router-link :to="{ name: 'viewGem', params: { gemId: gem.id } }"
+          >view</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,3 +26,39 @@ export default class GemsList extends Vue {
   created() {}
 }
 </script>
+
+<style lang="scss" scoped>
+.gemsList__container {
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  align-items: stretch;
+  align-content: stretch;
+
+  .gemsList__item {
+    // height: 70px;
+    min-width: 10vw;
+    max-width: 40vw;
+    min-height: 10vh;
+    min-height: 15vh;
+    padding: 0.3em;
+
+    p {
+      margin: 0.1em;
+    }
+    h2 {
+      margin: 0.3em 0;
+      font-size: 1.3em;
+    }
+
+    .gemsList__innerBox {
+      padding: 0.3em;
+      border: 1px solid gray;
+
+      .gemsList__tagsList {
+        font-size: 0.8em;
+      }
+    }
+  }
+}
+</style>
