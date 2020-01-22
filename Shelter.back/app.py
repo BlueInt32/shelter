17c48cdc87471891886f12f52ad220d2d812d7a6
BlueInt32 <simon.budin@gmail.com>
2020-01-22 19:21:37 +0100

    API - Add a shitty book boilerplate with sqlalchemy

--- Shelter.back/app.py
+++ Shelter.back/app.py
@@ -1,34 +1,65 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
-print(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Book
 
-gems = [
-    {
-        'id': 1,
-        'title': 'Gem number 1'
-    },
-    {
-        'id': 2,
-        'title': 'Gem number 2'
-    }
-]
+# Connect to Database and create database session
+engine = create_engine('sqlite:///books-collection.db')
+Base.metadata.bind = engine
 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# landing page that will display all the books in our database
+# This function operate on the Read operation.
 @app.route('/')
-def index():
-    return 'coucou'
+@app.route('/books')
+def show_books():
+    books = DBSession().query(Book).all()
+    return render_template("books.html", books=books)
 
-@app.route('/gems')
-def get_gems():
-    return {'gems':gems}
 
-@app.route('/gems/<int:gemId>')
-def get_gem_by_id(gemId):
-    return_value = {}
-    for gem in gems:
-        if gem["id"] == gemId:
-            return_value = gem
-            return return_value
+# This will let us Create a new book and save it in our database
+@app.route('/books/new/', methods=['GET', 'POST'])
+def new_book():
+    if request.method == 'POST':
+        newBook = Book(title=request.form['name'], author=request.form['author'], genre=request.form['genre'])
+        DBSession().add(newBook)
+        DBSession().commit()
+        return redirect(url_for('show_books'))
+    else:
+        return render_template('newBook.html')
 
-if __name__ == "__main__":
-    app.run(port=5001)
+
+# This will let us Update our books and save it in our database
+@app.route("/books/<int:book_id>/edit/", methods=['GET', 'POST'])
+def edit_book(book_id):
+    editedBook = DBSession().query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedBook.title = request.form['name']
+            return redirect(url_for('show_books'))
+    else:
+        return render_template('editBook.html', book=editedBook)
+
+
+# This will let us Delete our book
+@app.route('/books/<int:book_id>/delete/', methods=['GET', 'POST'])
+def delete_book(book_id):
+    bookToDelete = DBSession().query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        DBSession().delete(bookToDelete)
+        DBSession().commit()
+        return redirect(url_for('show_books', book_id=book_id))
+    else:
+        return render_template('deleteBook.html', book=bookToDelete)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    # app.run(host='0.0.0.0', port=4996)
