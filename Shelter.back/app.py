1a29453da68fab109249bef0afd2c51ad3258480
BlueInt32 <simon.budin@gmail.com>
2020-01-30 09:32:32 +0100

    API - Impl. almost functional tags and elements

--- Shelter.back/app.py
+++ Shelter.back/app.py
@@ -3,6 +3,8 @@ import logging
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import reqparse, Resource, Api, fields, marshal_with
 from flask_cors import CORS, cross_origin
+from datetime import datetime
+import sys
 
 logging.getLogger('flask_cors').level = logging.DEBUG
 
@@ -25,71 +27,95 @@ db = SQLAlchemy(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('title')
+parser.add_argument('text')
+parser.add_argument('tags', action='append')
 
-resource_fields = {
+tag_fields = {
+    'id': fields.Integer,
+    'label': fields.String
+}
+element_fields = {
     'id': fields.Integer,
     'title': fields.String,
-    'description': fields.String
+    'text': fields.String,
+    # 'creation_date': fields.DateTime,
+    # 'update_date': fields.DateTime,
+    'tags': fields.Nested(tag_fields)
 }
 
 
 class ElementApi(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(element_fields)
     def get(self, element_id):
         retrieved_element = Element.query.get(element_id)
         return retrieved_element
 
     def put(self, element_title):
         args = parser.parse_args(strict=True)
-        new_element = Element(title=args['title'].title)
+        new_element = Element(args['title'].title)
         db.session.add(new_element)
         db.session.commit()
         return new_element, 201
 
 
 class ElementsListApi(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(element_fields)
     def get(self):
-        #t.start('done and done')
+        # t.start('done and done')
         elements = db.session.query(Element).all()
-        #t.stop('done and done')
+        # t.stop('done and done')
         return elements
 
+    @marshal_with(element_fields)
     def post(self):
         args = parser.parse_args()
-        new_element = Element(title=args['title'], description='le trombonne')
-        db.session.add(new_element)
-        db.session.commit()
-        return new_element.id, 201
+        try:
+            new_tags = [Tag(a) for a in args.tags or []]
+            new_element = Element(args['title'], args['text'], new_tags)
+            db.session.add(new_element)
+            db.session.commit()
+            return new_element.id, 201
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            return 'Error', 400
+
+
+tags = db.Table('tags',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+                db.Column('element_id', db.Integer, db.ForeignKey('element.id'), primary_key=True),
+                )
 
 
 class Element(db.Model):
+    def __init__(self, title, text, input_tags):
+        self.title = title
+        self.text = text
+        self.creation_date = datetime.utcnow()
+        self.update_date = datetime.utcnow()
+        self.tags_associated = input_tags
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=False, nullable=False)
-    description = db.Column(db.String(200), unique=False, nullable=True)
+    text = db.Column(db.String(200), unique=False, nullable=True)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    update_date = db.Column(db.DateTime, nullable=False)
+    tags_associated = db.relationship('Tag', secondary=tags, lazy='subquery',
+                                      backref=db.backref('elements_associated', lazy=True))
+
+    # def __repr__(self):
+    #     return '<Element %r>' % self.title
+
+
+class Tag(db.Model):
+    def __init__(self, label):
+        self.label = label
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(200), unique=True, nullable=False)
 
 
 api.add_resource(ElementsListApi, '/api/elements')
 api.add_resource(ElementApi, '/api/elements/<element_id>')
-
-# landing page that will display all the books in our database
-# This function operate on the Read operation.
-# @app.route('/')
-# @app.route('/elements')
-# def show_books():
-#     print(__name__)
-#     elements = db.session.query(Element).all()
-#     return {'items': elements}
-#
-#
-# # This will let us Create a new book and save it in our database
-# @app.route('/elements', methods=['POST'])
-# def create_new_element():
-#     new_element = Element(title=request.form['title'], description=request.form['title'])
-#     db.session.add(new_element)
-#     db.session.commit()
-#     return {'created': new_element}
-
 
 if __name__ == '__main__':
     app.debug = True
--- Shelter.back/database_setup.py
+++ Shelter.back/database_setup.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import time
 
 app = Flask(__name__)
 
@@ -16,16 +18,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # silence the deprecation 
 
 db = SQLAlchemy(app)
 
+tags = db.Table('tags',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+                db.Column('element_id', db.Integer, db.ForeignKey('element.id'), primary_key=True),
+                )
+
 
 class Element(db.Model):
+    def __init__(self, title, text, input_tags):
+        self.title = title
+        self.text = text
+        self.creation_date = datetime.utcnow()
+        self.update_date = datetime.utcnow()
+        self.tags = input_tags
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=False, nullable=False)
-    description = db.Column(db.String(200), unique=False, nullable=True)
+    text = db.Column(db.String(200), unique=False, nullable=True)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    update_date = db.Column(db.DateTime, nullable=False)
+    tags_associated = db.relationship('Tag', secondary=tags, lazy='subquery',
+                                      backref=db.backref('elements_associated', lazy=True))
+
+    # def __repr__(self):
+    #     return '<Element %r>' % self.title
+
+
+class Tag(db.Model):
+    def __init__(self, label):
+        self.label = label
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(200), unique=True, nullable=False)
 
 
 @app.route('/element')
 def create_element():
-    user = Element(title="Coucou", description="this is a test")
+    user = Element(title="Coucou", text="this is a test", tags=[])
+
     db.session.add(user)
     db.session.commit()
     return {'id': user.id, 'name': user.name, 'job': user.job}
@@ -40,11 +70,21 @@ def reset_db():
     if database_exists(DB_URL):
         print('Deleting database.')
         status += 'delete '
+        # db.drop_all(bind=['element_tag', 'tag', 'element'])
+        db.drop_all()
         drop_database(DB_URL)
+    time.sleep(1)
     if not database_exists(DB_URL):
         print('Creating database.')
         status += 'create '
         create_database(DB_URL)
+        db.create_all()
 
-    db.create_all()
+    # db.create_all()
+
     return 'OK ' + status
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5001)
