048ba97dda005a81bca1ca7cc4f46d330682ac03
BlueInt32 <simon.budin@gmail.com>
2020-02-08 09:22:29 +0100

    UI - Move menu to separate vue file

--- Shelter.front/src/App.vue
+++ Shelter.front/src/App.vue
@@ -1,64 +1,24 @@
 <template>
   <div id="layout" class="content pure-g">
-    <div id="nav" class="pure-u">
-      <a href="#" class="nav-menu-button">Menu</a>
-      <div class="nav-inner">
-        <!-- <button class="primary-button pure-button">Create</button> -->
-        <router-link to="/add" class="primary-button pure-button"
-          >Add</router-link
-        >
-
-        <div class="pure-menu">
-          <ul class="pure-menu-list">
-            <li class="pure-menu-item">
-              <router-link to="/" class="pure-menu-link"
-                >All elements</router-link
-              >
-              <!-- <a href="#" class="pure-menu-link"
-                >All elements <span class="email-count">(2)</span></a
-              > -->
-            </li>
-            <li class="pure-menu-item">
-              <a href="#" class="pure-menu-link">Manage labels</a>
-            </li>
-            <li class="pure-menu-heading">Labels</li>
-            <li class="pure-menu-item">
-              <a href="#" class="pure-menu-link"
-                ><span class="email-label-personal"></span>Personal</a
-              >
-            </li>
-            <li class="pure-menu-item">
-              <a href="#" class="pure-menu-link"
-                ><span class="email-label-work"></span>Work</a
-              >
-            </li>
-            <li class="pure-menu-item">
-              <a href="#" class="pure-menu-link"
-                ><span class="email-label-travel"></span>Travel</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <!-- <div class="pure-menu pure-menu-horizontal">
-      <router-link to="/" class="pure-menu-heading pure-menu-link"
-        >Shelter</router-link
-      >
-      <ul class="pure-menu-list">
-        <li class="pure-menu-item">
-          <router-link to="/add" class="pure-menu-link">Add</router-link>
-        </li>
-        <li class="pure-menu-item">
-          <router-link to="/about" class="pure-menu-link">About</router-link>
-        </li>
-      </ul>
-    </div> -->
+    <!-- <a href="#menu" id="menuLink" class="menu-link pure-u">
+      <span></span>
+    </a> -->
+    <Menu />
     <router-view />
-    <vue-snotify></vue-snotify>
+    <vue-snotify />
   </div>
 </template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import Menu from '@/views/Menu.vue';
 
+@Component({
+  components: {
+    Menu
+  }
+})
+export default class App extends Vue {}
+</script>
 <style lang="scss">
 @import './styles/custom.scss';
 @import 'purecss';
---
+++ Shelter.front/src/views/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="menu" class="pure-u">
+    <div class="pure-menu">
+      <a class="pure-menu-heading" href="#">Shelter</a>
+
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+          <router-link to="/add" class="pure-menu-link">Add</router-link>
+        </li>
+        <li class="pure-menu-item">
+          <router-link to="/" class="pure-menu-link">All elements</router-link>
+          <!-- <a href="#" class="pure-menu-link"
+                >All elements <span class="email-count">(2)</span></a
+              > -->
+        </li>
+        <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link">Manage labels</a>
+        </li>
+        <!-- <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link"
+            ><span class="email-label-personal"></span>Personal</a
+          >
+        </li>
+        <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link"
+            ><span class="email-label-work"></span>Work</a
+          >
+        </li>
+        <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link"
+            ><span class="email-label-travel"></span>Travel</a
+          >
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+export default class Menu extends Vue {}
+</script>
+
+<style></style>
