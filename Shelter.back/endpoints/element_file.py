a684662a3c0d95aaa192736fecabbbec59da7bca
BlueInt32 <simon.budin@gmail.com>
2020-02-14 06:21:32 +0100

    API - Allow image direct view

--- Shelter.back/endpoints/element_file.py
+++ Shelter.back/endpoints/element_file.py
@@ -8,10 +8,14 @@ from services.tags_service import resolve_tags
 class ElementFileApi(Resource):
   def get(self, element_id):
     retrieved_element = Element.query.get_or_404(element_id)
-    return send_file(
+    response = send_file(
       io.BytesIO(retrieved_element.attached_file),
       as_attachment= True,
       attachment_filename=retrieved_element.title+'.jpg',
       # attachment_filename='logo.jpeg',
       mimetype='image/jpg'
     )
+    # this header makes browser display image directly instead of
+    # suggesting download popup
+    response.headers['Content-disposition'] = 'inline'
+    return response
